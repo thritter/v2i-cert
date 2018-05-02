3d41aef3494793f00ff541ce3fa54748f5abd758
Thomas Ritter <thomas.x.ritter@siemens.com>
2018-05-02 08:24:36 +0200

    Fix OPR_BV_01

--- 001-TP-RSU-SNMP-OPR-BV-01/run_test.sh
+++ 001-TP-RSU-SNMP-OPR-BV-01/run_test.sh
@@ -4,3 +4,30 @@
 # MIB to produce a complete list of all supported MIBs and OIDs and the current setting
 # for each Object. Also, verify that MIB changes are retained after SUT power is cycled.
 
+echo "Execute TP-RSU-SNMP-OPR-BV-01"
+
+#set -x
+TOPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+source ${TOPDIR}/common.sh
+
+echo "Populate tables"
+snmpset ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}.4.1.11.0 i 4
+snmpset ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}.5.1.7.0 i 4
+snmpset ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}.7.1.11.0 i 4
+snmpset ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}.13.1.8.0 i 4
+
+
+echo "Walk tree IP MIB"
+snmpwalk ${RW_AUTH_ARGS} ${SUT_IP} ${MGMT_MIB_2}.4
+
+echo "Walk tree RSU"
+snmpwalk ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}
+
+
+echo "Clear tables"
+snmpset ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}.4.1.11.0 i 6
+snmpset ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}.5.1.7.0 i 6
+snmpset ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}.7.1.11.0 i 6
+snmpset ${RW_AUTH_ARGS} ${SUT_IP} ${RSU_MIB}.13.1.8.0 i 6
+
+echo "TP-RSU-SNMP-OPR-BV-01 succeeded"
